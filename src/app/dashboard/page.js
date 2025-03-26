68a89bd3c019fcaaf352d2306504f45400f26083
sidney <shogo12000@hotmail.com>
2025-03-26 08:35:08 -0700

    first

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,8 @@
+export default function UserDashboard() {
+    return (
+      <main className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-2xl font-bold mb-4">Bem-vindo ao Dashboard do Usuário</h1>
+        <p>Esta é a página do usuário.</p>
+      </main>
+    );
+  }
