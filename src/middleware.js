3f7ba373f5408b21e2c39a4b051f20af5a5202a9
sidney <shogo12000@hotmail.com>
2025-03-26 23:46:45 -0700

    first

--- src/middleware.js
+++ src/middleware.js
@@ -1,35 +1,38 @@
-import { NextResponse } from "next/server";
-import { jwtVerify } from "jose";
-import { cookies } from "next/headers";
+// import { NextResponse } from "next/server";
+// import { jwtVerify } from "jose";
+// import { cookies } from "next/headers";
 
-const JWT_SECRET = process.env.JWT_SECRET || "seu_segredo_super_secreto";
+// const JWT_SECRET = process.env.JWT_SECRET || "seu_segredo_super_secreto";
 
-export async function middleware(req) {
-  const cookieStore =  cookies();
-  const token = cookieStore.get("auth_token");
-  console.log(token);
-  console.log("middleware 0");
-  if (!token) {
-    console.log("Token não encontrado, redirecionando para /login");
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-  console.log("middleware 1");
-  try {
-    // Verifica o token JWT usando jose
-    const { payload } = await jwtVerify(token, new TextEncoder().encode(JWT_SECRET));
-    console.log("Token verificado com sucesso:", payload);
+// export async function middleware(req) {
+//   const cookieStore =  cookies();
+//   const token = cookieStore.get("auth_token")?.value;
+//   const teste = cookieStore.get('auth_token').value;
+//   console.log(teste);
+//   console.log(token);
+//   console.log("middleware 0");
 
-    // Configura cabeçalhos para evitar cache
-    const response = NextResponse.next();
-    response.headers.set("Cache-Control", "no-store");
-    return response;
-  } catch (error) {
-    console.error("Erro na verificação do token:", error.message);
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-}
+//   if (!token) {
+//     console.log("Token não encontrado, redirecionando para /login");
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
+//   console.log("middleware 1");
+//   try {
+//     // Verifica o token JWT usando jose
+//     const { payload } = await jwtVerify(token, new TextEncoder().encode(JWT_SECRET));
+//     console.log("Token verificado com sucesso:", payload);
 
-// Aplica o middleware apenas em páginas protegidas
-export const config = {
-  matcher: ["/dashboard/:path*"],
-};
+//     // Configura cabeçalhos para evitar cache
+//     const response = NextResponse.next();
+//     response.headers.set("Cache-Control", "no-store");
+//     return response;
+//   } catch (error) {
+//     console.error("Erro na verificação do token:", error.message);
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
+// }
+
+// // Aplica o middleware apenas em páginas protegidas
+// export const config = {
+//   matcher: ["/dashboard/:path*"],
+// };
