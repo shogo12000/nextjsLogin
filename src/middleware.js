ef88cbef1643a976335f9ba559e0ba234f0f8be3
sidney <shogo12000@hotmail.com>
2025-03-27 00:59:35 -0700

    first

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 import { cookies } from "next/headers";
 
-// const JWT_SECRET = process.env.JWT_SECRET || "seu_segredo_super_secreto";
+const JWT_SECRET = process.env.JWT_SECRET || "seu_segredo_super_secreto";
 
 // export async function middleware(req) {
 //   const cookieStore =  cookies();
@@ -10,7 +10,7 @@ import { cookies } from "next/headers";
 
 //   console.log(token);
 //   console.log("middleware 0");
-  
+
 //   if (!token) {
 //     console.log("Token não encontrado, redirecionando para /login");
 //     return NextResponse.redirect(new URL("/login", req.url));
@@ -41,6 +41,13 @@ export const config = {
 };
 
 export async function middleware(req) {
+  const cookieStore = cookies();
+  const token = cookieStore.get("auth_token")?.value;
+
+  console.log(token);
+  console.log("middleware 0");
+
+
   console.log("Middleware executado para:", req.nextUrl.pathname);
   return NextResponse.next();
 }
